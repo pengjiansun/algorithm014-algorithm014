ccc503e9578c1325332a41d061a0acd5755f5ef9
孙鹏建 <pengjiansun@163.com>
2020-08-23 13:48:06 +0800

    有效的字母异位词：
    （1）两个字符串是异位词，当且仅当排序后完全相同：
    暴力解法，先排序，再比较。
    （2）两个字符串是异位词，当且仅当每个字母出现的频次相同：
    使用哈希表统计字母出现次数，比较哈希表是否相同。考虑到只涉及小写字母，范围有限，可以使用字母计数器作为哈希表。

--- Week_02/242. Valid Anagram/Solution3.java
+++ Week_02/242. Valid Anagram/Solution3.java
@@ -3,11 +3,9 @@ package com.sunpj.algorithms.leetcode.isanagram;
 import java.util.Arrays;
 
 /*
- * 解法2：使用哈希表
+ * 解法23：优化的哈希表解法
  * 考虑到字符范围是a~z，可以使用数组作为计数器，实现一个简化的哈希表
- * 实际有两种实现方法：
- * 方法1是首先遍历s和t进行increment和decrement计数，然后检查counter。
- * 方法2是首先遍历s进行increment，然后遍历t，进行decrement，如果出现计数小于0的字母，说明t中出现了新的字母，或者同一个字母，在t中出现
+ * 使用一个计数器，首先遍历s进行increment，然后遍历t，进行decrement，如果出现计数小于0的字母，说明t中出现了新的字母，或者同一个字母，在t中出现
  * 的次数超过了s中出现的次数。
  * 时间复杂度：O(n)
  * 空间复杂度：O(1)
